53e957a32c958bd556732427026f85c94593e644
Salvador Bravo <sa.bravo0901@gmail.com>
2026-05-05 18:18:58 -0400

    inicializacion react - js

--- Fase 2/Evidencias Proyecto/Evidencias de Sistema Aplicacion/frontend/src/routes.tsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router";
-import Login from "./pages/Login";
-
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: Login
-  }
-]);
